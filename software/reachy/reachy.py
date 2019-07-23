9ea9a25f04d11affc69fb20d21a53941daa237ee
Pierre Rouanet <pierre.rouanet@gmail.com>
2019-07-23 17:21:20 +0200

    Add head integration in Reachy.

--- software/reachy/reachy.py
+++ software/reachy/reachy.py
@@ -1,10 +1,11 @@
-from .parts import LeftArm, RightArm
+from .parts import LeftArm, RightArm, Head
 
 
 class Reachy(object):
     def __init__(self,
                  left_arm=None,
-                 right_arm=None):
+                 right_arm=None,
+                 head=None):
 
         if left_arm is not None and not isinstance(left_arm, LeftArm):
             raise ValueError('"left_arm" must be a LeftArm or None!')
@@ -13,3 +14,7 @@ class Reachy(object):
         if right_arm is not None and not isinstance(right_arm, RightArm):
             raise ValueError('"right_arm" must be a RightArm or None!')
         self.right_arm = right_arm
+
+        if head is not None and not isinstance(head, Head):
+            raise ValueError('"head" must be a Head or None!')
+        self.head = head
