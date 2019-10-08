674277f000a5232d0fa8f841ed7783e803718887
Pierre Rouanet <pierre.rouanet@gmail.com>
2019-10-08 16:01:07 +0200

    Add parts list to robot.

--- software/reachy/reachy.py
+++ software/reachy/reachy.py
@@ -9,17 +9,40 @@ class Reachy(object):
                  right_arm=None,
                  head=None):
 
-        if left_arm is not None and not isinstance(left_arm, LeftArm):
+        self._parts = []
+
+        if left_arm is not None:
+            if not isinstance(left_arm, LeftArm):
             raise ValueError('"left_arm" must be a LeftArm or None!')
+            self._parts.append(left_arm)
+            if left_arm.hand is not None:
+                self._parts.append(left_arm.hand)
         self.left_arm = left_arm
 
-        if right_arm is not None and not isinstance(right_arm, RightArm):
+        if right_arm is not None:
+            if not isinstance(right_arm, RightArm):
             raise ValueError('"right_arm" must be a RightArm or None!')
+            self._parts.append(right_arm)
+            if right_arm.hand is not None:
+                self._parts.append(right_arm.hand)
         self.right_arm = right_arm
 
-        if head is not None and not isinstance(head, Head):
+        if head is not None:
+            if not isinstance(head, Head):
             raise ValueError('"head" must be a Head or None!')
+            self._parts.append(head)
         self.head = head
+
+        logger.info(
+            'Connected to reachy',
+            extra={
+                'parts': [p.name for p in self.parts]
+            }
+        )
+
+    @property
+    def parts(self):
+        return self._parts
 
     def goto(self,
              goal_positions, duration,
