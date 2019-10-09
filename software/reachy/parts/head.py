9b5c92e9b363acd22a8775995d52f636016b16cd
Pierre Rouanet <pierre.rouanet@gmail.com>
2019-10-09 17:47:06 +0200

    Change head part to use orbita.

--- software/reachy/parts/head.py
+++ software/reachy/parts/head.py
@@ -3,16 +3,17 @@ from ..io import SharedLuosIO
 
 
 class Head(ReachyPart):
-    dxl_motors = {
-        'pan': {'id': 8, 'offset': 90.0, 'orientation': 'direct'},
-        'tilt': {'id': 5, 'offset': 0.0, 'orientation': 'direct'},
+    orbital_config = {
+        'Pc_z': [0, 0, 117.4],
+        'Cp_z': [0, 0, 84.256],
+        'R': 51.56,
     }
 
     def __init__(self, camera_id, luos_port):
         ReachyPart.__init__(self, name='head')
 
         self.luos_io = SharedLuosIO(luos_port)
-        self.attach_dxl_motors(self.luos_io, Head.dxl_motors)
+        self.attach_orbita_actuator('neck', self.luos_io, Head.orbital_config)
 
         # We import vision here to avoid OpenCV ImportError issue
         # if we are not using the Head part.
