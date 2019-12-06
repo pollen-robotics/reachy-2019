ed4e8ba84c3cb021b901e99f232555990fbdab2c
Pierre Rouanet <pierre.rouanet@gmail.com>
2019-12-06 14:35:17 +0100

    Typo in error log.

--- software/reachy/parts/arm.py
+++ software/reachy/parts/arm.py
@@ -22,7 +22,7 @@ class Arm(ReachyPart):
         self.attach_dxl_motors(self.luos_io, dxl_motors)
 
         if hand is not None and hand not in hands.keys():
-            raise ValueError('"hand" must be one of {hands.keys()} or None!')
+            raise ValueError(f'"hand" must be one of {list(hands.keys())} or None!')
 
         if hand is not None:
             hand_part = hands[hand](luos_port=self.luos_io.port)
