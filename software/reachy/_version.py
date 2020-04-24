a98ccbfc6a6262a28127512eefca8258d3d77c70
Pierre Rouanet <pierre.rouanet@gmail.com>
2020-04-24 11:08:15 +0200

    Update to version 1.0.0.

--- software/reachy/_version.py
+++ software/reachy/_version.py
@@ -1,2 +1,2 @@
 """Version module."""
-__version__ = '1.0.0a4'
+__version__ = '1.0.0'
--- software/reachy/parts/__init__.py
+++ software/reachy/parts/__init__.py
@@ -8,13 +8,4 @@ Used to define all parts in Reachy:
 """
 
 from .arm import LeftArm, RightArm # noqa
-
-try:
-    from .head import Head # noqa
-except ImportError:
-    class Head(object):
-        """Placeholder Head part if OpenCV not found."""
-
-        def __init__(self, *args, **kwargs):
-            """Raise Error message if OpenCV not installed."""
-            raise NotImplementedError('Install opencv if you want to use head!')
+from .head import Head # noqa
