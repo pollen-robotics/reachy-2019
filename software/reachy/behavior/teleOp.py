0ce3b43434ede46003e1439fb7088a8ddbe0d53d
sgriffault <simon.griffault@etu.u-bordeaux.fr>
2020-07-27 14:22:02 +0200

    pydocstyle

--- software/reachy/behavior/teleOp.py
+++ software/reachy/behavior/teleOp.py
@@ -6,14 +6,11 @@ from threading import Thread
 
 
 class TeleOp(object):
-    """Class to be use by tele-operation applications
+    """Class to be use by tele-operation applications.
 
     Args:
-
         reachy (:py:class:`~reachy.Reachy`): robot which will follow the movements
-
         left_arm (reachy.parts.LeftArm): left arm part if present or None if absent
-
         right_arm (reachy.parts.RightArm): right arm part if present or None if absent
 
     Provides easy-to-use method to make reachy follow hands position, rotation and hand opening.
@@ -26,10 +23,9 @@ class TeleOp(object):
         self.is_moving = False
 
     def start_moving(self, wait=0):
-        """Move along with VR poses of both hands.
+        """Create thread and call the follow_movements method.
 
         Args:
-
             wait : float
                 time in seconds to wait before start to move
         """
@@ -42,12 +38,14 @@ class TeleOp(object):
         self.move_t.start()
 
     def stop_moving(self):
+        """Make reachy stop moving."""
         self.is_moving = False
 
         if hasattr(self, 'move_t') and self.move_t.is_alive():
             self.move_t.join()
 
     def follow_movements(self):
+        """Move along with VR poses of both hands."""
         self.update_state(self.right_arm.io.ws.state_dict)
         is_left_hand_opened = self.left_hand_command
         is_right_hand_opened = self.right_hand_command
@@ -58,13 +56,11 @@ class TeleOp(object):
             is_right_hand_opened = self.move_arm(self.right_arm, self.right_hand_command, is_right_hand_opened)
 
     def move_arm(self, arm, hand_command, is_hand_opened):
-        """
+        """Move one arm according to the pose given by update_state.
+
         Args:
-
             arm (reachy.parts.arm): the arm to move
-
             hand_command (bool): true if the hand is supposed to be opened/closed according to the state received by the webSocket, false otherwise
-
             is_hand_opened (bool): the current hand's state
 
         The boleans arguments are used to check wether the state has changed or not. It allows us not to call the open()/close() method when
@@ -94,7 +90,6 @@ class TeleOp(object):
         """Update the fields used in the followMovements() and move_arm() mehods.
 
         Args:
-
         state_dict (dictionnary) : contains all the informations to make both hands moving.
         It is updated in the webSocket sync loop.
         """
@@ -121,15 +116,19 @@ class TeleOp(object):
             raise NameError('NoHandInformationsInWebSocket')
 
     def goto_right_arm_joint_solution(self, joint_solution, duration, wait):
+        """Move reachy's right arm according to given joints."""
         for joint_pos, motor in zip(joint_solution, self.right_arm.motors):
             motor.goto(joint_pos, duration)
 
     def goto_left_arm_joint_solution(self, joint_solution, duration, wait):
+        """Move reachy's left arm according to given joints."""
         for joint_pos, motor in zip(joint_solution, self.left_arm.motors):
             motor.goto(joint_pos, duration)
 
     def open_hand(self, arm):
+        """Open Reachy's hand."""
         arm.hand.open()
 
     def close_hand(self, arm):
+        """Close Reachy's hand."""
         arm.hand.close()
